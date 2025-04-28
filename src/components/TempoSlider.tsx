7272e834dd348382760024e3c5b44af31a9df841
Thomas Moody <tjmoody18@gmail.com>
2025-04-27 20:03:00 -0500

    Add half and double buttons to tempo

--- src/components/TempoSlider.tsx
+++ src/components/TempoSlider.tsx
@@ -26,19 +26,32 @@ const TempoSlider: React.FC<TempoSliderProps> = ( {
     setTempo(tempo => Math.min(tempo + 1, MAX_TEMPO))
   }
 
+  const halfTempo = () => {
+    setTempo(tempo => Math.max(tempo / 2, MIN_TEMPO))
+  }
+
+  const doubleTempo = () => {
+    setTempo(tempo => Math.min(tempo * 2, MAX_TEMPO))
+  }
+
+
+
   return (
     <div className="tempo-container">
       <div className="tempo-label">{tempo} bpm</div>
       <div className="tempo-flex-container">
+      <button onClick={halfTempo}>0.5x</button>
         <button onClick={decrementTempo}>-</button>
         <input 
           type="range"
           min={`${MIN_TEMPO}`}
           max={`${MAX_TEMPO}`}
+          step="5"
           value={tempo}
           onChange={handleChange}
         />
         <button onClick={incrementTempo}>+</button>
+        <button onClick={doubleTempo}>2x</button>
        </div>
     </div>
   )
